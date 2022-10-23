f314a4079b68c1efea8765b4e34bcedcfdac4054
Mahinkahn <mahinkahn37@gmail.com>
2022-10-23 20:24:34 +0600

    kaj sas

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-light mb-5">
                 <div className="container-fluid d-flex justify-content-between">
-                    <Link className="navbar-brand" to="/"><h1 className='font-weight-bold'>Quiz And Ans</h1></Link>
+                    <Link className="navbar-brand" to="/"><h1 className='font-weight-bold'>Quiz & Ans</h1></Link>
                     <div className="">
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
